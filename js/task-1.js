1e955e3a5c2fbeb4ebabeab0938fb4f335278dc9
chasov-artem <chasov90@gmail.com>
2024-05-24 17:08:19 +0300

    task-2

--- js/task-1.js
+++ js/task-1.js
@@ -8,7 +8,3 @@ console.log(makeTransaction(3, 1000, 15000));
 console.log(makeTransaction(10, 5000, 8000));
 console.log(makeTransaction(8, 2000, 10000));
 console.log(makeTransaction(10, 500, 5000));
-// 1. оголосити змінну загальної суми замовлення (вартість всіх дроїдів)
-// 2. додати перевірку, чи може клієнт оплатити замовлення:
-// якщо ні, то повертає рядок "Insufficient funds!"
-// в інакшому випадку: "You ordered <quantity> droids worth <totalPrice> credits!"
---
+++ js/task-2.js
@@ -0,0 +1,11 @@
+function formatMessage(message, maxLength) {
+    const messageLength = message.length;
+    return messageLength <= maxLength ? `${message}` : `${message.slice(0, maxLength)}...`
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16));
+console.log(formatMessage("Curabitur ligula sapien", 23));
+console.log(formatMessage("Vestibulum facilisis purus nec", 20));
+console.log(formatMessage("Vestibulum facilisis purus nec", 30));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
